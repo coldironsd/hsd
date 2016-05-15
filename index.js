d512ea65939950e0491f9d1f98f9c0c4e3901b8b
JackDarth <jacky.won@gmail.com>
2016-05-15 12:07:17 -0400

    kitten

--- index.js
+++ index.js
@@ -39,35 +39,36 @@ app.post('/webhook', function (req, res) {
             //     sendMessage(event.sender.id, message);
             // }
             
-            if (sender_listener.whatQuestion(event.sender.id, event.message.text) != false){
+            // if (sender_listener.whatQuestion(event.sender.id, event.message.text) != false){
 
-                message = sender_listener.whatQuestion(event.sender.id, event.message.text);
-                sendMessage(event.sender.id, message);
-            }else if (sender_listener.whenQuestion(event.sender.id, event.message.text) != false){
+            //     message = sender_listener.whatQuestion(event.sender.id, event.message.text);
+            //     sendMessage(event.sender.id, message);
+            // }else if (sender_listener.whenQuestion(event.sender.id, event.message.text) != false){
 
-                message = sender_listener.whenQuestion(event.sender.id, event.message.text);
-                sendMessage(event.sender.id, message);
-            }else if (sender_listener.whereQuestion(event.sender.id, event.message.text) != false){
+            //     message = sender_listener.whenQuestion(event.sender.id, event.message.text);
+            //     sendMessage(event.sender.id, message);
+            // }else if (sender_listener.whereQuestion(event.sender.id, event.message.text) != false){
 
-                message = sender_listener.whereQuestion(event.sender.id, event.message.text)
-                sendMessage(event.sender.id, message);
-            }else if (sender_listener.howMuchQuestion(event.sender.id, event.message.text) != false){
+            //     message = sender_listener.whereQuestion(event.sender.id, event.message.text)
+            //     sendMessage(event.sender.id, message);
+            // }else if (sender_listener.howMuchQuestion(event.sender.id, event.message.text) != false){
 
-                message = sender_listener.howMuchQuestion(event.sender.id, event.message.text);
-                sendMessage(event.sender.id, message);
-            }else if (carrier_listener.fromWhereQuestion(event.sender.id, event.message.text) != false){
+            //     message = sender_listener.howMuchQuestion(event.sender.id, event.message.text);
+            //     sendMessage(event.sender.id, message);
+            // }else if (carrier_listener.fromWhereQuestion(event.sender.id, event.message.text) != false){
 
-                message = carrier_listener.fromWhereQuestion(event.sender.id, event.message.text);
-                sendMessage(event.sender.id, message);
-            }else if (carrier_listener.toWhereQuestion(event.sender.id, event.message.text) != false){
+            //     message = carrier_listener.fromWhereQuestion(event.sender.id, event.message.text);
+            //     sendMessage(event.sender.id, message);
+            // }else if (carrier_listener.toWhereQuestion(event.sender.id, event.message.text) != false){
 
-                message = carrier_listener.toWhereQuestion(event.sender.id, event.message.text);
-                sendMessage(event.sender.id, message);
-            }else if (carrier_listener.feeQuestion(event.sender.id, event.message.text) != false){
+            //     message = carrier_listener.toWhereQuestion(event.sender.id, event.message.text);
+            //     sendMessage(event.sender.id, message);
+            // }else if (carrier_listener.feeQuestion(event.sender.id, event.message.text) != false){
 
-                message = carrier_listener.feeQuestion(event.sender.id, event.message.text);
-                sendMessage(event.sender.id, message);
-            }else if (app_listener.roleQuestion(event.sender.id, event.message.text) != false){
+            //     message = carrier_listener.feeQuestion(event.sender.id, event.message.text);
+            //     sendMessage(event.sender.id, message);
+            // }else 
+            if (app_listener.roleQuestion(event.sender.id, event.message.text) != false){
 
                 message = app_listener.roleQuestion(event.sender.id, event.message.text);
                 sendMessage(event.sender.id, message);
