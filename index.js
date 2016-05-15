152c9e8a90577949e58391e7082c02c5641752e0
JackDarth <jacky.won@gmail.com>
2016-05-15 11:29:48 -0400

    kitten

--- index.js
+++ index.js
@@ -33,9 +33,10 @@ app.post('/webhook', function (req, res) {
         if (event.message && event.message.text) {
             
             test = app_listener.roleQuestion(event.sender.id, event.message.text);
-            sendMessage(event.sender.id, {text: "Echo: " + test});
+            var json = JSON.parse(test);
+            sendMessage(event.sender.id, {text: "Echo: " + json.text});
             if(test !== false){
-                sendMessage(recipientId, test);
+                sendMessage(recipientId, json.text);
             }
             // if (sender_listener.whatQuestion(event.sender.id, event.message.text)){}
             // else if (sender_listener.whenQuestion(event.sender.id, event.message.text)){}
